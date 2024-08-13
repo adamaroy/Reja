1056bafad89c26b977aad38576a406a79f20f4f8
ADAM <azizbekrustamovic@gmail.com>
2024-08-14 02:28:05 +0900

    fix: Train.js qoshildi

---
+++ train.js
@@ -0,0 +1,35 @@
+//A TASK
+
+
+console.log("WEb serverini boshlash");
+const express =require("express");
+
+const app = express();
+const http = require('http');
+
+//1 : KIRISH CODELARI
+app.use(express.static("public"));                  //kirib kelayotgan har bir request uchun public folder ochiq
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));      //formdan biror narsa post qilsa express server qabul qiladi (yozilmasa express ignore qiladi)
+
+//2 :Session ga bogliq codelar
+
+//3: Views ga bogliq codelar
+app.set("views","views");
+app.set("view engine","ejs");                       //ejs orqali HTML frontend yasaladi backend ichida
+
+//4:Routinga bogliq codelar
+app.get("/hello",function(req ,res){
+    res.end("<h1>Hello world by Adam!</h1>");
+});
+app.get("/gift",function(req ,res){
+    res.end("<h1>GIFT!</h1>");
+});
+
+
+const server = http.createServer(app);
+let PORT =3000;
+server.listen(PORT,function(){
+    console.log(`the server is running successfully on port : ${PORT}`);
+    
+})
