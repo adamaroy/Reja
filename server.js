3bb1cf293fdc4e7a0fd13ebf27938753701a8471
ADAM <azizbekrustamovic@gmail.com>
2024-08-14 03:36:40 +0900

    fix: harid.ejs qoshildi

--- server.js
+++ server.js
@@ -16,12 +16,23 @@ app.set("views","views");
 app.set("view engine","ejs");                       //ejs orqali HTML frontend yasaladi backend ichida
 
 //4:Routinga bogliq codelar
-app.get("/hello",function(req ,res){
-    res.end("<h1>Hello world by Adam!</h1>");
+app.post("/create-item",(req,res)=> {   //post databasega malumotni ozi bilan olib kelib va db ga yozadi
+console.log(req.body);
+res.json({test:"succes"});
+
 });
-app.get("/gift",function(req ,res){
-    res.end("<h1>GIFT!</h1>");
+
+app.get("/",function (req, res) {    //get databasedan malumot olib oqish uchun ishlatadi
+    res.render ("harid");
 });
+
+
+// app.get("/hello",function(req ,res){
+//     res.end("<h1>Hello world by Adam!</h1>");
+// });
+// app.get("/gift",function(req ,res){
+//     res.end("<h1>GIFT!</h1>");
+// });
 
 
 const server = http.createServer(app);
--- train.js
+++ train.js
@@ -1,6 +1,4 @@
-//A TASK
-
-
+// B TASK
 console.log("WEb serverini boshlash");
 const express =require("express");
 
@@ -19,12 +17,23 @@ app.set("views","views");
 app.set("view engine","ejs");                       //ejs orqali HTML frontend yasaladi backend ichida
 
 //4:Routinga bogliq codelar
-app.get("/hello",function(req ,res){
-    res.end("<h1>Hello world by Adam!</h1>");
+app.post("/create-item",(req,res)=> {   //post databasega malumotni ozi bilan olib kelib va db ga yozadi
+console.log(req.body);
+res.json({test:"succes"});
+
 });
-app.get("/gift",function(req ,res){
-    res.end("<h1>GIFT!</h1>");
+
+app.get("/",function (req, res) {    //get databasedan malumot olib oqish uchun ishlatadi
+    res.render ("harid");
 });
+
+
+// app.get("/hello",function(req ,res){
+//     res.end("<h1>Hello world by Adam!</h1>");
+// });
+// app.get("/gift",function(req ,res){
+//     res.end("<h1>GIFT!</h1>");
+// });
 
 
 const server = http.createServer(app);
@@ -33,3 +42,41 @@ server.listen(PORT,function(){
     console.log(`the server is running successfully on port : ${PORT}`);
     
 })
+
+
+
+// A TASK
+
+
+// console.log("WEb serverini boshlash");
+// const express =require("express");
+
+// const app = express();
+// const http = require('http');
+
+// //1 : KIRISH CODELARI
+// app.use(express.static("public"));                  //kirib kelayotgan har bir request uchun public folder ochiq
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));      //formdan biror narsa post qilsa express server qabul qiladi (yozilmasa express ignore qiladi)
+
+// //2 :Session ga bogliq codelar
+
+// //3: Views ga bogliq codelar
+// app.set("views","views");
+// app.set("view engine","ejs");                       //ejs orqali HTML frontend yasaladi backend ichida
+
+// //4:Routinga bogliq codelar
+// app.get("/hello",function(req ,res){
+//     res.end("<h1>Hello world by Adam!</h1>");
+// });
+// app.get("/gift",function(req ,res){
+//     res.end("<h1>GIFT!</h1>");
+// });
+
+
+// const server = http.createServer(app);
+// let PORT =3000;
+// server.listen(PORT,function(){
+//     console.log(`the server is running successfully on port : ${PORT}`);
+    
+// })
