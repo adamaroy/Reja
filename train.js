957d881d9046a5f556348bcdb048c9a338943d96
ADAM <azizbekrustamovic@gmail.com>
2024-08-21 02:17:35 +0900

    feat: A TASK

--- train.js
+++ train.js
@@ -1,41 +1,62 @@
-console.log("Jack Ma maslahatlari");
-
-const list = [
-    "yaxshi talaba boling!",  // 0-20
-    "Togri boshliq tanlang!", // 20-30
-    "Ozingizga ishlashni boshlang!", //30-40
-    "siz kuchli bolgan narsalarni qiling", //40-50
-    "yoshlarga invest qiling!", //50-60
-    "endi dam oling!" //60~
-];
-//SYNCHRONOUS FUNCTION
-
-function maslahatBering(a, callback) {
-    if (typeof a !== 'number') callback("Insert number!", null);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-    else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-    else {
-        setInterval(function() {
-        // setTimeout(function() {
-            callback(null,list[5]);
-        }, 5000);
-        
+// A TASK
+function countLetterOccurrences(letter, word) {
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            count++;
+        }
     }
+    return count;
 }
-console.log('passed here 0');
 
-maslahatBering(85, (err, data) => {
-    if (err) console.log("ERROR:", err);
-    else{
-    console.log('javob:', data);
-    }
 
-});
-console.log('passed here 1');
+const letterNumber = countLetterOccurrences("a", "adam");
+console.log("letter num:",letterNumber);  
+
+
+
+
+// console.log("Jack Ma maslahatlari");
+
+// const list = [
+//     "yaxshi talaba boling!",  // 0-20
+//     "Togri boshliq tanlang!", // 20-30
+//     "Ozingizga ishlashni boshlang!", //30-40
+//     "siz kuchli bolgan narsalarni qiling", //40-50
+//     "yoshlarga invest qiling!", //50-60
+//     "endi dam oling!" //60~
+// ];
+// //SYNCHRONOUS FUNCTION
+// //CALL BACK
+// function maslahatBering(a, callback) {
+//     if (typeof a !== 'number') callback("Insert number!", null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         setInterval(function() {
+//         // setTimeout(function() {
+//             callback(null,list[5]);
+//         }, 5000);
+        
+//     }
+// }
+// console.log('passed here 0');
+
+// maslahatBering(85, (err, data) => {
+//     if (err) console.log("ERROR:", err);
+//     else{
+//     console.log('javob:', data);
+//     }
+
+// });
+// console.log('passed here 1');
+
+
 //ASYNCHRONOUS FUNCTION
+
 
 // async function maslahatBering(a) {
 //     if (typeof a !== 'number') throw new Error("Insert number!");
@@ -61,7 +82,7 @@ console.log('passed here 1');
 //     }
 // }
 
-// then catch methodlari!
+                        // then catch methodlari! //CALL VIA THEN CATCH
 
 // console.log('passed here 0');
 
@@ -72,6 +93,8 @@ console.log('passed here 1');
     
 // })
 // console.log('passed here 1');
+
+                        //CALL VIA ASYNC/AWAIT
 
 // async function run(){
 //     let javob = await maslahatBering(70);
